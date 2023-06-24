babd8b1d74f0afc34d1bb324da64235cf07d5e1f
Veervijay <virajrana123@gmail.com>
2023-06-24 15:13:12 -0400

    Set home tableViewCell and it's collectionView

---
+++ Netflix Clone/Controllers/SearchViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SearchViewController.swift
+//  Netflix Clone
+//
+//  Created by Shivduttsinh Mahida on 6/23/23.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        view.backgroundColor = .systemBackground
+    }
+
+}
--- Netflix Clone/Controllers/UpcomingViewController.swift
+++ Netflix Clone/Controllers/UpcomingViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  UpcomingViewController.swift
 //  Netflix Clone
 //
 //  Created by Shivduttsinh Mahida on 6/23/23.
@@ -7,13 +7,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class UpcomingViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
+        view.backgroundColor = .systemBackground
+        
     }
-
+    
 
 }
-
