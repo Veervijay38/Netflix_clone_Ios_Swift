ffee2ab782e5824bdeebd1417a4a26786538ebed
Veervijay <virajrana123@gmail.com>
2023-06-25 16:25:17 -0400

    Titletableview for upcoming view controller

---
+++ Netflix Clone/ViewModels/TitleViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  TitleViewModel.swift
+//  Netflix Clone
+//
+//  Created by Shivduttsinh Mahida on 6/25/23.
+//
+
+import Foundation
+
+struct TitleViewModel {
+    let titleName: String
+    let posterURL: String
+}
